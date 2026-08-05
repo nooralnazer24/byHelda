487643afa7521d95459bf9c34a82a80e22188a20
nooralnazer24 <nalnazer537@gmail.com>
2026-08-05 23:24:17 +0300

    Fix complete anon key in supabase.ts

--- artifacts/byhellda/src/lib/supabase.ts
+++ artifacts/byhellda/src/lib/supabase.ts
@@ -4,3 +4,4 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://xjozbweincrtaa
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhqb3pid2VpbmNxcnRhYWJ3eHV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNzQ0NTEsImV4cCI6MjEwMDc1MDQ1MX0.fZ334izhEK3EDiZiNi7FIzWEotCGIESs6BXv20peVSU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
